217ab1b1127f3355a467f4dfdf58fc894d533441
Nataliya <mostovanatalya12@gmail.com>
2023-02-16 14:33:40 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a newline
+ * main - prints "Programming is like building a multilingual 
+ * puzzle, followed by a newline
  * return 0.
  */
 
@@ -10,6 +11,6 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 
 }
