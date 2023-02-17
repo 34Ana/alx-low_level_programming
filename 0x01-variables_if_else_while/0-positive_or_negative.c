4778ccaf6b21ea1a92967633dad25ad1966dc533
Nataliya <mostovanatalya12@gmail.com>
2023-02-17 02:31:12 -0800

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int num = n
+	if(n<0;)
+	
+	{printf("%d\n";"-n"; a++);
+}
+	else
+{
+	printf("%d\n";"n"; a++);
+}
+	else
+{
+	printf("%d\n";"n=0"; a++);
+
+} 
+return 0;
+}
+
+
