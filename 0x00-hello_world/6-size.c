7d181e2d5f8d11b30a02cbfa65b891df923ec33a
Nataliya <mostovanatalya12@gmail.com>
2023-02-20 01:26:37 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,11 @@
 /**
  * main - list the 32 and 36 system bits sizes
  * Return 0
- * /
- *
- * int main();
- * {
- * printf("Size of char: %d byte(s)\n", sizeof(char));
+ */
+
+int main();
+{
+printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
