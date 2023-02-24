94545ed2c2d12c6b95c8d37f3d2111604e206404
Nataliya <mostovanatalya12@gmail.com>
2023-02-24 10:31:07 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
-int main()
+int main(void)
 {
-printf("_putchar\n");
+putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 return:0;
 }
