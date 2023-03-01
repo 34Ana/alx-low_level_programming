edd85866b79e0ffd49dd242ece3286aaa9407d3c
Nataliya <mostovanatalya12@gmail.com>
2023-02-28 16:14:13 -0800

    11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,45 +3,44 @@
 #include <time.h>
 
 /**
- * main - generade random passwords for
+ * main - generate random passwords for
  * program 101-crackme.
  * Return: 0.
  */
-
 int main(void)
 {
-char password[84];
-int index = 0, sum = 0, diff_half1, deff_half2;
-srand(time(0));
-while (sum < 2772)
-{
-password[index] = 33 + rand() % 94;
-sum += password[index++];
+    char password[84];
+    int index = 0, sum = 0, diff_half1 = 0, diff_half2 = 0;
+    srand(time(0));
+    while (sum < 2772)
+    {
+        password[index] = 33 + rand() % 94;
+        sum += password[index++];
+    }
+    password[index] = '\0';
+    if (sum != 2772)
+    {
+        int diff = (sum - 2772) / 2;
+        if ((sum - 2772) % 2 != 0)
+            diff++;
+        for (index = 0; password[index]; index++)
+        {
+            if (password[index] >= (33 + diff))
+            {
+                password[index] -= diff;
+                break;
+            }
+        }
+        for (index = 0; password[index]; index++)
+        {
+            if (password[index] >= (33 + diff))
+            {
+                password[index] -= diff;
+                break;
+            }
+        }
+    }
+    printf("%s", password);
+    return (0);
 }
-password[index] = '\0';
-if (sum != 2772)
-{
-diff_half1 = (sum - 2772) / 2;
-diff_half2 = (sum - 2772) / 2;
-if ((sum - 2772) % 2 != 0)
-diff_half1++;
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half1))
-{
-password[index] -= diff_half1;
-break;
-}
-}
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half2));
-{
-password[index] -= diff_half2;
-break;
-}
-}
-}
-printf("%s", password);
-return (0);
-}
+
