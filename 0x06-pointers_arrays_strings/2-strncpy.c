84f3497b7686b3465d51d19df9312b7c8ff40e3f
Nataliya <mostovanatalya12@gmail.com>
2023-03-01 13:40:44 -0800

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * @dest: copies to.
  * @src: copies from.
  * @n: the variable.
- * Return: (dest).
+ * Return: 0.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - compares two strings.
+ * @s1: s1.
+ * @s2: s2.
+ * Return: 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+
