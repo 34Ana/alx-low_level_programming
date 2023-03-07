8f93ebcb5f5e68036cc56d5592dcb1f5f1d7ffaf
Nataliya <mostovanatalya12@gmail.com>
2023-03-07 11:40:08 -0800

    6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: 2d array representing the chessboard
+ * Return: chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+int row, col;
+for (row = 0; row < 8; row++)
+{
+	for (col - 0; col < 8; col++)
+	{
+		putchar(a[row][col]);
+	}
+	putchar('\n');
+}
+}
