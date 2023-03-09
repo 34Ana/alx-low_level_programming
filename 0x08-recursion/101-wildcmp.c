fbe6a1433df27c85b4b638a7ac5e5a2087dba7cf
Nataliya <mostovanatalya12@gmail.com>
2023-03-08 23:12:24 -0800

    8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
  * wildcmp - compares two strings and returns 1 if the strings
  * can be considered identical, otherwise return 0.
@@ -8,25 +7,30 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0')
-{
-while (*s2 == '*')
-{
-s2++;
+    if (*s1 == '\0')
+    {
+        if (*s2 == '\0')
+            return (1);
+        else if (*s2 == '*')
+            return (wildcmp(s1, s2 + 1));
+        else
+            return (0);
+    }
+    else if (*s2 == '\0')
+    {
+        return (0);
+    }
+    else if (*s2 == '*')
+    {
+        return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+    }
+    else if (*s1 == *s2 || *s2 == '?')
+    {
+        return (wildcmp(s1 + 1, s2 + 1));
+    }
+    else
+    {
+        return (0);
+    }
 }
-return (*s2 == '\0');
-}
-if (*s2 == '\0')
-{
-return (*s1 == '\0');
-}
-if (*s2 == '*')
-{
-return (wildcmp(s1, s2 + 1) || wildcmp(s1 +1, s2));
-}
-if (*s1 == *s2 || *s2 == '?')
-{
-return wildcmp(s1 + 1, s2 + 1);
-}
-return (0);
-}
+
