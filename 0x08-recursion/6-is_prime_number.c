9b10b65198b2d97df4b4f1bf02e36f2c7ed82278
Nataliya <mostovanatalya12@gmail.com>
2023-03-09 00:57:25 -0800

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,7 @@ int check_prime(int n, int i);
 */
 int is_prime_number(int n)
 {
-return check_prime(n, 2);
+return (check_prime(n, 2));
 }
 /**
 * check_prime - check if a number is prime
@@ -21,10 +21,10 @@ return check_prime(n, 2);
 int check_prime(int n, int i)
 {
 if (n <= 1)
-return 0;
+return (0);
 if (i * i > n)
-return 1;
+return (1);
 if (n % i == 0)
-return 0;
-return check_prime(n, i + 1);
+return (0);
+return (check_prime(n, i + 1));
 }
