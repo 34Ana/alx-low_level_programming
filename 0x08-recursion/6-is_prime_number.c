5671997bb783ea68d3eae21c63b4b94e797faf47
Nataliya <mostovanatalya12@gmail.com>
2023-03-09 00:42:05 -0800

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,28 +4,27 @@
 int check_prime(int n, int i);
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number
- * @n: number to check for primality
- * Return: 1 if prime, 0 otherwise
- */
+* is_prime_number - returns 1 if the input integer is a prime number
+* @n: number to check for primality
+* Return: 1 if prime, 0 otherwise
+*/
 int is_prime_number(int n)
 {
-        return check_prime(n, 2);
+return check_prime(n, 2);
 }
-
 /**
- * check_prime - check if a number is prime
- * @n: number to check
- * @i: iteration times
- * Return: 1 if prime, 0 otherwise
- */
+* check_prime - check if a number is prime
+* @n: number to check
+* @i: iteration times
+* Return: 1 if prime, 0 otherwise
+*/
 int check_prime(int n, int i)
 {
-        if (n <= 1)
-                return 0;
-        if (i * i > n)
-                return 1;
-        if (n % i == 0)
-                return 0;
-        return check_prime(n, i + 1);
+if (n <= 1)
+return 0;
+if (i * i > n)
+return 1;
+if (n % i == 0)
+return 0;
+return check_prime(n, i + 1);
 }
