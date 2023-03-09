eef579c7c9259432d1e339ee99db495e6933b14d
Nataliya <mostovanatalya12@gmail.com>
2023-03-08 23:58:53 -0800

    7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,25 +7,27 @@
  */
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursions(s + 1));
+        int len = strlen(s);
+        if (len <= 1)
+                return (1);
+        else
+                return (comparator(s, 0, len - 1));
 }
+
 /**
  * comparator - compares each character of the string
  * @s: the string
  * @n1: smallest iterator
  * @n2: largest iterator
- * Return: .
+ * Return: 1 if palindrome, 0 if not
  */
 int comparator(char *s, int n1, int n2)
 {
-	if (*(s + n1) == *(s + n2))
-	{
-		if ( n1 == n2 || n1 == n2 +1)
-			return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
-	}
-	return (0);
+        if (*(s + n1) == *(s + n2))
+        {
+                if (n1 == n2 || n1 == n2 + 1)
+                        return (1);
+                return (comparator(s, n1 + 1, n2 - 1));
+        }
+        return (0);
 }
