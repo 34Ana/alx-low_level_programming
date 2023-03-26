adc5774f105838dc8fed8d3837f53e6cfa651e19
Nataliya <mostovanatalya12@gmail.com>
2023-03-26 02:31:32 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,18 +6,13 @@
  */
 int main(void)
 {
-	char letter = 'a';
-	while (letter <= 'z')
-	{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-		letter++;
-	}
-	char upletter = 'A';
-	while (upletter <= 'Z')
-	{
-		putchar(upletter);
-		upletter++;
-	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	
 	putchar('\n');
 	return (0);
 }
